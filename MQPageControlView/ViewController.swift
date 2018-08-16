80b3470e85195cc87203ecc7712fd33f13de977c
AidyBao <aidybao555@sina.com>
2018-08-16 11:07:26 +0800

    1.0.0
    swift 版本的Banner滚动视图

--- MQPageControlView/ViewController.swift
+++ MQPageControlView/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pageControlView: ZXPageControlView!
+    
+    var dataSoure = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pageControlView.flipInterval = 3 // Default 2
+        self.pageControlView.delegate = self
+        self.pageControlView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+extension ViewController: ZXPageControlViewDataSource {
+    func zxPageControlView(_ scrollView: ZXPageControlView, pageAt index: Int) -> UIView {
+        let imageV = UIImageView()
+        imageV.backgroundColor = UIColor.groupTableViewBackground
+        if self.dataSoure.count > 0 {
+            let imgUrl = self.dataSoure[index]
+//            imageV.kf.setImage(with: URL(string: imgUrl), placeholder: nil, options: nil, progressBlock: nil, completionHandler: nil)
+        }
+        return imageV
+    }
+    
+    
+    func numberofPages(_ inScrollView: ZXPageControlView) -> Int {
+//        return dataSoure.count
+        return 5
+    }
+}
+
+extension ViewController:ZXPageControlViewDelegate {
+    func zxAutoScrolView(_ scrollView: ZXPageControlView, selectAt index: Int) {
+        
+    }
+}
+
 
